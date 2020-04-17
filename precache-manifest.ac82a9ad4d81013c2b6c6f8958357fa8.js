fe355755d7b9c837daa63cd70178db89eb8cf862
sunchaofei <sunchaofei@didichuxing.com>
2020-04-17 18:40:43 +0800

    Updates

---
+++ precache-manifest.ac82a9ad4d81013c2b6c6f8958357fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b20e868bb389624ecb75fbc621e2b26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "508e356c0ac156273508",
+    "url": "./static/css/main.ef083d1e.chunk.css"
+  },
+  {
+    "revision": "d684a03143f33abbd916",
+    "url": "./static/js/2.6b1acfdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6b1acfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508e356c0ac156273508",
+    "url": "./static/js/main.fb27f5b6.chunk.js"
+  },
+  {
+    "revision": "9b357398b6fc3ee34318",
+    "url": "./static/js/runtime-main.5db12b80.js"
+  }
+]);
